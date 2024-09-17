7edd30c12816445d7fd5b1b28e01dd80ec760e32
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-09-17 14:07:22 +0000

    Att1

--- TAD/LISTA/LSE.c
+++ TAD/LISTA/LSE.c
@@ -17,3 +17,38 @@ void inicializar(LSE *l){
     l->fim = NULL;
     l->qtd = 0;
 }
+
+int isEmpty( LSE *l){
+    return l->inicio == NULL;
+}
+
+void inserirFinal(LSE *l,int valor){
+    ListNode *novo = malloc(sizeof(ListNode));
+    novo->info = valor;
+    novo->prox = NULL;
+    if (isEmpty(l)) {
+        l->inicio = novo;
+    } else {
+        l->fim->prox = novo;
+    }
+    l->fim = novo;
+}
+
+void inserirInicio(LSE *l, int valor){
+    ListNode *novo = malloc(sizeof(ListNode));
+    novo->info = valor;
+    novo->prox = NULL;
+    if(isEmpty )
+}
+
+void removerInicio(LSE *l){
+
+}
+
+void removerUltimo(LSE *l){
+
+}
+
+void list(LSE l){
+
+}
