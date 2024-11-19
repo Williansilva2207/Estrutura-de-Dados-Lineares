7e5e5ec436cd596620d9787b57765279199bb904
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-11-19 23:52:48 +0000

    REVISIONQUESTION4

--- TAD/LISTA/LISTA DUPLAMENTE ENCADEADA ORDENADA/ATVD OF REVISION/LISTA DE REVISÃO/QUESTION4/LDECircOrdenad.h
+++ TAD/LISTA/LISTA DUPLAMENTE ENCADEADA ORDENADA/ATVD OF REVISION/LISTA DE REVISÃO/QUESTION4/LDECircOrdenad.h
@@ -30,6 +30,14 @@ void inicializar(LDECirc *list){
     list->qtd = 0;
 }
 
+int isEmpty(LDECirc list){
+    if(list.qtd == 0){
+        return TRUE;
+    }else{
+        return FALSE;
+    }
+}
+
 LDENode *criarNo(char name[], int matricula, double media, int faltas){
     LDENode *novo = (LDENode*) malloc(sizeof(LDENode));
     novo->prox = NULL;
@@ -39,4 +47,57 @@ LDENode *criarNo(char name[], int matricula, double media, int faltas){
     novo->info->media = media;
     novo->info->falta = faltas;
     return novo;
+}
+
+void inserirOrdenado(char name[],int matricula, double media, int faltas, LDECirc *list){
+    LDENode *novo;
+    if(isEmpty(*list)==TRUE){
+        novo = criarNo(name, matricula, media, faltas);
+        list->inicio = novo;
+        list->fim = novo;
+        novo->prox = novo;
+        novo->ant = novo;
+        list->qtd = 1;
+    }else if(strcmp(name,list->inicio->info->nome) == 0){
+        printf("Aluno ja esta registrado.\n");
+    }else if(strcmp(name,list->inicio->info->nome) < 0){
+        novo = criarNo(name, matricula, media, faltas);
+        novo->prox = list->inicio;
+        list->inicio->ant = novo;
+        list->inicio = novo;
+        list->fim->prox = list->inicio;
+        list->inicio->ant = list->fim;
+        list->qtd ++;
+    }else if(strcmp(name,list->fim->info->nome) == 0){
+        printf("Aluno ja esta registrado.\n");
+    }else if(strcmp(name,list->fim->info->nome) > 0){
+        novo = criarNo(name, matricula, media, faltas);
+        list->fim->prox = novo;
+        novo->ant = list->fim;
+        list->fim = novo;
+        list->fim->prox = list->inicio;
+        list->inicio->ant = list->fim;
+        list->qtd ++;
+    }else{
+        LDENode *aux;
+        LDENode *bef;
+        aux = list->inicio->prox;
+        while(TRUE){
+            if(strcmp(name, aux->info->nome)==0){
+                printf("Aluno ja esta registrado.\n");
+                return;
+            }else if(strcmp(name, aux->info->nome) < 0){
+                novo = criarNo(name, matricula, media, faltas);
+                bef = aux->ant;
+                novo->prox = aux;
+                aux->ant = novo;
+                bef->prox = novo;
+                novo->ant = bef;
+                list->qtd ++;
+                return;
+            }else{
+                aux = aux->prox;
+            }
+        }
+    }
 }
